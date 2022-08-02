ab260609e57ba0e4932207727bb453352422b320
Vladimir Tyan <tyanv01@gmail.com>
2022-08-02 19:38:24 +0300

    Исправил функцию перевода в словарь

--- file_work.py
+++ file_work.py
@@ -3,20 +3,28 @@ file_name = 'data.txt'
 def catalog_reader(file_name):
     menu = {}
     cook_book = {}
-    list_ingr = []
+
     keys = ['ingredient', 'quantity', 'measure' ]
     with open(file_name, encoding='utf-8') as file:
         for line in file:
+            list_ingr = []
             cook = line.strip()
-            for i in range(int(file.readline())):
+            circle_number = int(file.readline())
+            for i in range(circle_number):
                 ingr = file.readline().strip()
                 menu = ingr.split('|')
                 menu_dict = dict(zip(keys, menu))
                 list_ingr.append(menu_dict)
-                print(list_ingr)
             cook_book[cook] = list_ingr
             file.readline()
     return cook_book
         
-# print(catalog_reader(file_name))
-catalog_reader(file_name)
+print(catalog_reader(file_name))
+# catalog_reader(file_name)
+
+# get_shop_list_by_dishes(['Запеченый картофель', 'Омлет'], 2)
+
+
+# {
+#    'Картофель': {'measure': 'кг', 'quantity': 2}   
+# }
