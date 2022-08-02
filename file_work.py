e884225c115443c44b0bbace54a8a98e7ca011d3
Vladimir Tyan <tyanv01@gmail.com>
2022-08-02 22:17:25 +0300

    Добавил функцию для расчета покупок. Пока с одним блюдом

--- file_work.py
+++ file_work.py
@@ -15,14 +15,34 @@ def catalog_reader(file_name):
                 menu = ingr.split('|')
                 menu_dict = dict(zip(keys, menu))
                 list_ingr.append(menu_dict)
-            cook_book[cook] = list_ingr
+            cook_book[cook] = list_ingr                
             file.readline()
     return cook_book
         
 print(catalog_reader(file_name))
 # catalog_reader(file_name)
 
-# get_shop_list_by_dishes(['Запеченый картофель', 'Омлет'], 2)
+def get_shop_list_by_dishes(menu, person):
+# Функция рассчитывает список покупок, в зависимости от меню и кол-ва человек
+    cook_book = catalog_reader(file_name)
+    shop_list = {}
+    if menu in cook_book:
+        list_ingr = cook_book[menu]
+          
+        for i in list_ingr: 
+            shop_quantity = {}
+            shop_quantity['measure'] = i['measure']
+            shop_quantity['quantity'] = int(i['quantity'])*person
+            shop_list[i['ingredient']] = shop_quantity
+        return shop_list
+            
+    else:
+        print("Такого блюда нет в меню")      
+
+
+# get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+# print(get_shop_list_by_dishes('Запеченный картофель', 4))
 
 
 # {
